b393be05835f569760fe46bab94c06f1b345a593
Zhuravlev Dmitry <qwerty-91@list.ru>
2024-01-11 15:36:52 +0500

    Initial Commit

---
+++ FC System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FC System
+//
+//  Created by Zhuravlev Dmitry on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
