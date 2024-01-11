b41c003ce3ffa13b77e52cc3b00a79ddf97fa2be
Zhuravlev Dmitry <qwerty-91@list.ru>
2024-01-11 16:38:16 +0500

    add TabBar and MoreVC

--- FC System/SceneDelegate.swift
+++ FC System/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let tabBarController = ViewController()
+        let tabBarController = TabBarController()
         let navigationController = UINavigationController(rootViewController: tabBarController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
---
+++ FC System/TabBarController.swift
@@ -0,0 +1,60 @@
+//
+//  TabBarController.swift
+//  FC System
+//
+//  Created by Zhuravlev Dmitry on 11.01.2024.
+//
+
+import Foundation
+import UIKit
+
+
+final class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        setupTabBarController()
+        setupTabBarViewController()
+    }
+
+    private func setupTabBarController() {
+
+        tabBar.tintColor = .systemRed
+        tabBar.backgroundColor = .gray
+
+    }
+
+    private func setupTabBarViewController() {
+        let first = MainViewController()
+        let firstIcon = UITabBarItem(title: "Главная", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        first.tabBarItem = firstIcon
+
+        let second = MainViewController()
+        let secondIcon = UITabBarItem(title: "Турнир", image: UIImage(systemName: "trophy"), selectedImage: UIImage(systemName: "trophy.fill"))
+        second.tabBarItem = secondIcon
+
+        let third = MainViewController()
+        let thirdIcon = UITabBarItem(title: "Команда", image: UIImage(systemName: "person.3"), selectedImage: UIImage(systemName: "person.3.fill"))
+        third.tabBarItem = thirdIcon
+
+        let fourth = MainViewController()
+        let fourthIcon = UITabBarItem(title: "Тренировки", image: UIImage(systemName: "sportscourt"), selectedImage: UIImage(systemName: "sportscourt.fill"))
+        fourth.tabBarItem = fourthIcon
+
+        let fifth = MoreViewController()
+        let fifthIcon = UITabBarItem(title: "Еще", image: UIImage(systemName: "ellipsis.rectangle"), selectedImage: UIImage(systemName: "ellipsis.rectangle.fill"))
+        fifth.tabBarItem = fifthIcon
+
+        let controllers = [first, second, third, fourth, fifth]
+        self.setViewControllers(controllers, animated: true)
+
+
+    }
+
+
+    // MARK: - Navigation
+
+
+}
+
